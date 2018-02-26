fef8f76bc74d60d709c7df01977e1aa9821f15d9
Shoaib Jalal <shoaib.jalal@myy.haaga-helia.fi>
2018-02-26 14:15:05 +0200

    Making responsive adjustments

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,13 +2,16 @@ import React, {Fragment} from 'react';
 
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Fragment>
-    <Toolbar/>
-    <main className = {classes.Content}>
-        {props.children}
-    </main>
+        <Toolbar/>
+        <SideDrawer/>
+        
+        <main className = {classes.Content}>
+            {props.children}
+        </main>
     </Fragment>
 
 );
